617b1c858ceee96c7a6810040a203901b31bb644
Yuichiro Terada <yuichiro_terada@caddi.jp>
2020-11-19 01:23:02 +0900

    KdTree

--- bin/test.rs
+++ bin/test.rs
@@ -25,7 +25,7 @@ fn main() {
     }
     {
         //let kdtree = kd_tree::kd::KdTree::<_, [f64; 3], _>::sort_by_key(
-        let kdtree = kd_tree::kd::KdTreeRef3::sort_points_by(&mut points, OrderedFloat);
+        let kdtree = kd_tree::kd::KdTree3::sort_points_by(&mut points, OrderedFloat);
         let now = std::time::Instant::now();
         for p in kdtree.items() {
             //let nearest = kd_tree::kd::kd_find_nearest(&points, |p, k| p[k], p);
--- src/kd.rs
+++ src/kd.rs
@@ -41,13 +41,49 @@ pub fn kd_sort_by<T>(
     recurse(items, 0, dim, kd_compare);
 }
 
-pub struct KdTreeRef<'a, T, N>(&'a [T], PhantomData<N>);
-pub type KdTreeRef2<'a, T> = KdTreeRef<'a, T, typenum::U2>;
-pub type KdTreeRef3<'a, T> = KdTreeRef<'a, T, typenum::U3>;
-impl<'a, T, N: typenum::Unsigned + typenum::NonZero> KdTreeRef<'a, T, N> {
-    pub fn items(&self) -> &'a [T] {
-        self.0
+pub trait KdCollection {
+    type Item;
+    fn items(&self) -> &[Self::Item];
+}
+impl<'a, T> KdCollection for &'a [T] {
+    type Item = T;
+    fn items(&self) -> &[Self::Item] {
+        self
     }
+}
+impl<T> KdCollection for Vec<T> {
+    type Item = T;
+    fn items(&self) -> &[Self::Item] {
+        self
+    }
+}
+
+pub struct KdTree<Collection, N>(Collection, PhantomData<N>);
+pub type KdTree2<Collection> = KdTree<Collection, typenum::U2>;
+pub type KdTree3<Collection> = KdTree<Collection, typenum::U3>;
+
+impl<C: KdCollection, N> KdTree<C, N> {
+    pub fn items(&self) -> &[C::Item] {
+        self.0.items()
+    }
+    pub fn nearest(
+        &self,
+        query: &impl Point<Scalar = <C::Item as Point>::Scalar, Dim = <C::Item as Point>::Dim>,
+    ) -> Nearest<C::Item, <C::Item as Point>::Scalar>
+    where
+        C::Item: Point,
+    {
+        kd_nearest(self.items(), query)
+    }
+    pub fn nearest_by<Q: Point>(
+        &self,
+        query: &Q,
+        coord: impl Fn(&C::Item, usize) -> Q::Scalar + Copy,
+    ) -> Nearest<C::Item, Q::Scalar> {
+        kd_nearest_by(self.items(), query, coord)
+    }
+}
+impl<'a, T, N: typenum::Unsigned + typenum::NonZero> KdTree<&'a [T], N> {
     pub fn sort_points(points: &'a mut [T]) -> Self
     where
         T: Point<Dim = N>,
@@ -72,22 +108,6 @@ impl<'a, T, N: typenum::Unsigned + typenum::NonZero> KdTreeRef<'a, T, N> {
     pub fn sort_by(items: &'a mut [T], compare: impl Fn(&T, &T, usize) -> Ordering + Copy) -> Self {
         kd_sort_by(items, N::to_usize(), compare);
         Self(items, PhantomData)
-    }
-    pub fn nearest(
-        &self,
-        query: &impl Point<Scalar = T::Scalar, Dim = T::Dim>,
-    ) -> Nearest<'a, T, T::Scalar>
-    where
-        T: Point,
-    {
-        kd_nearest(self.0, query)
-    }
-    pub fn nearest_by<Q: Point>(
-        &self,
-        query: &Q,
-        coord: impl Fn(&T, usize) -> Q::Scalar + Copy,
-    ) -> Nearest<'a, T, Q::Scalar> {
-        kd_nearest_by(self.0, query, coord)
     }
 }
 
