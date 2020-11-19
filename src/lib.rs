63335666a5515f23f5b89ca4122b9cee17e9e76b
Yuichiro Terada <yuichiro_terada@caddi.jp>
2020-11-19 11:29:26 +0900

    KdTree<Vec<T>, N>

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,11 @@ pub use nearest::*;
 pub use sort::*;
 use std::cmp::Ordering;
 use std::marker::PhantomData;
+use typenum::Unsigned;
 
 pub trait KdPoint {
     type Scalar: num_traits::NumAssign + Copy + PartialOrd;
-    type Dim: typenum::Unsigned + typenum::NonZero;
+    type Dim: Unsigned;
     fn dim() -> usize {
         <Self::Dim as typenum::Unsigned>::to_usize()
     }
@@ -31,11 +32,9 @@ impl<T> KdCollection for Vec<T> {
     }
 }
 
-pub struct KdTree<Collection, N>(Collection, PhantomData<N>);
-pub type KdTree2<Collection> = KdTree<Collection, typenum::U2>;
-pub type KdTree3<Collection> = KdTree<Collection, typenum::U3>;
+pub struct KdTree<Collection: KdCollection, N: Unsigned>(Collection, PhantomData<N>);
 
-impl<C: KdCollection, N> KdTree<C, N> {
+impl<C: KdCollection, N: Unsigned> KdTree<C, N> {
     pub fn items(&self) -> &[C::Item] {
         self.0.items()
     }
@@ -56,7 +55,37 @@ impl<C: KdCollection, N> KdTree<C, N> {
         kd_nearest_by(self.items(), query, coord)
     }
 }
-impl<'a, T, N: typenum::Unsigned + typenum::NonZero> KdTree<&'a [T], N> {
+
+impl<'a, T, N: Unsigned> KdTree<&'a [T], N> {
+    pub fn into_slice(self) -> &'a [T] {
+        self.0
+    }
+
+    pub fn sort_by<F>(items: &'a mut [T], compare: F) -> Self
+    where
+        F: Fn(&T, &T, usize) -> Ordering + Copy,
+    {
+        kd_sort_by(items, N::to_usize(), compare);
+        Self(items, PhantomData)
+    }
+
+    pub fn sort_by_key<Key: Ord, F>(items: &'a mut [T], kd_key: F) -> Self
+    where
+        F: Fn(&T, usize) -> Key + Copy,
+    {
+        Self::sort_by(items, |item1, item2, k| {
+            kd_key(item1, k).cmp(&kd_key(item2, k))
+        })
+    }
+
+    pub fn sort_points_by<Key: Ord, F>(points: &'a mut [T], f: F) -> Self
+    where
+        T: KdPoint<Dim = N>,
+        F: Fn(T::Scalar) -> Key,
+    {
+        Self::sort_by_key(points, |item, k| f(item.at(k)))
+    }
+
     pub fn sort_points(points: &'a mut [T]) -> Self
     where
         T: KdPoint<Dim = N>,
@@ -64,27 +93,65 @@ impl<'a, T, N: typenum::Unsigned + typenum::NonZero> KdTree<&'a [T], N> {
     {
         Self::sort_by_key(points, |item, k| item.at(k))
     }
-    pub fn sort_points_by<Key: Ord>(points: &'a mut [T], f: impl Fn(T::Scalar) -> Key) -> Self
-    where
-        T: KdPoint<Dim = N>,
-    {
-        Self::sort_by_key(points, |item, k| f(item.at(k)))
+}
+
+impl<T, N: Unsigned> KdTree<Vec<T>, N> {
+    pub fn into_vec(self) -> Vec<T> {
+        self.0
     }
-    pub fn sort_by_key<Key: Ord>(
-        items: &'a mut [T],
-        kd_key: impl Fn(&T, usize) -> Key + Copy,
-    ) -> Self {
-        Self::sort_by(items, |item1, item2, k| {
+
+    pub fn construct_by<F>(mut items: Vec<T>, compare: F) -> Self
+    where
+        F: Fn(&T, &T, usize) -> Ordering + Copy,
+    {
+        kd_sort_by(&mut items, N::to_usize(), compare);
+        Self(items, PhantomData)
+    }
+
+    pub fn construct_by_key<Key, F>(items: Vec<T>, kd_key: F) -> Self
+    where
+        Key: Ord,
+        F: Fn(&T, usize) -> Key + Copy,
+    {
+        Self::construct_by(items, |item1, item2, k| {
             kd_key(item1, k).cmp(&kd_key(item2, k))
         })
     }
-    pub fn sort_by(items: &'a mut [T], compare: impl Fn(&T, &T, usize) -> Ordering + Copy) -> Self {
-        kd_sort_by(items, N::to_usize(), compare);
-        Self(items, PhantomData)
+
+    pub fn from_points_by<Key: Ord, F>(points: Vec<T>, f: F) -> Self
+    where
+        T: KdPoint<Dim = N>,
+        F: Fn(T::Scalar) -> Key,
+    {
+        Self::construct_by_key(points, |item, k| f(item.at(k)))
+    }
+
+    pub fn from_points(points: Vec<T>) -> Self
+    where
+        T: KdPoint<Dim = N>,
+        T::Scalar: Ord,
+    {
+        Self::construct_by_key(points, |item, k| item.at(k))
     }
 }
 
-macro_rules! impl_points {
+pub type KdTreeRef<'a, T, N> = KdTree<&'a [T], N>;
+pub type KdTreeVec<T, N> = KdTree<Vec<T>, N>;
+
+macro_rules! define_kdtree_aliases {
+    ($($dim:literal),*) => {
+        $(
+            paste::paste! {
+                pub type [<KdTree $dim>]<Collection> = KdTree<Collection, typenum::[<U $dim>]>;
+                pub type [<KdTreeRef $dim>]<'a, T> = KdTree<&'a [T], typenum::[<U $dim>]>;
+                pub type [<KdTreeVec $dim>]<T> = KdTree<Vec<T>, typenum::[<U $dim>]>;
+            }
+        )*
+    };
+}
+define_kdtree_aliases!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+
+macro_rules! impl_kd_points {
     ($($len:literal),*) => {
         $(
             paste::paste!{
@@ -97,5 +164,4 @@ macro_rules! impl_points {
         )*
     };
 }
-
-impl_points!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+impl_kd_points!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
